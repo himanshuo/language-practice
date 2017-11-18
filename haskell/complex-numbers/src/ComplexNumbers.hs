c7af24254a9fe1545c5cb2ba1edb3025ad6ca84a
himanshu ojha <himano@amazon.com>
2017-11-18 09:18:59 -0500

    complex numbers

---
+++ haskell/complex-numbers/src/ComplexNumbers.hs
@@ -0,0 +1,45 @@
+module ComplexNumbers
+(Complex,
+ conjugate,
+ abs,
+ real,
+ imaginary,
+ mul,
+ add,
+ sub,
+ div,
+ complex) where
+
+import Prelude hiding (div, abs)
+
+-- Data definition -------------------------------------------------------------
+data Complex a = Complex a a deriving(Eq, Show)
+
+complex :: (a, a) -> Complex a
+complex (real, imaginary) = Complex real imaginary
+
+-- unary operators -------------------------------------------------------------
+conjugate :: Num a => Complex a -> Complex a
+conjugate (Complex r i) = Complex r (-i)
+
+abs :: Floating a => Complex a -> a
+abs (Complex r i) = sqrt(r^2 + i^2)
+
+real :: Num a => Complex a -> a
+real (Complex r _) = r
+
+imaginary :: Num a => Complex a -> a
+imaginary (Complex _ i) = i
+
+-- binary operators ------------------------------------------------------------
+mul :: Num a => Complex a -> Complex a -> Complex a
+mul (Complex a b) (Complex c d) = Complex (a * c - b * d) (b * c + a * d)
+
+add :: Num a => Complex a -> Complex a -> Complex a
+add (Complex a b) (Complex c d) = Complex (a + c) (b + d)
+
+sub :: Num a => Complex a -> Complex a -> Complex a
+sub (Complex a b) (Complex c d) = Complex (a - c) (b - d)
+
+div :: Fractional a => Complex a -> Complex a -> Complex a
+div (Complex a b) (Complex c d) = Complex ((a * c + b * d)/(c^2 + d^2)) ((b * c - a * d)/(c^2 + d^2))
