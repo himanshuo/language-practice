20731d26899e9d48b3df7caf0b7953c01918c3a8
himanshu ojha <himanshuo@gmail.com>
2017-11-19 18:13:15 -0500

    nucleotide count

---
+++ haskell/nucleotide-count/src/DNA.hs
@@ -0,0 +1,21 @@
+module DNA (nucleotideCounts) where
+
+import Data.Map
+
+-- when using Either, convention is that Left is error, Right is desired value
+
+nucleotideCounts :: String -> Either String (Map Char Int)
+nucleotideCounts "" = Right $ fromList [('A',0), ('C',0), ('G', 0), ('T', 0)]
+nucleotideCounts (x:xs) = 
+    let countsForRest = nucleotideCounts xs in
+    case countsForRest of 
+        Left e -> Left e
+        Right mapForRest -> 
+            let currentVal = Data.Map.lookup x mapForRest in 
+            case currentVal of 
+                Nothing -> Left "Invalid DNS Strand"
+                Just oldCount -> 
+                    let updatedMap = insert x (oldCount+1) mapForRest in 
+                        Right updatedMap
+
+    
