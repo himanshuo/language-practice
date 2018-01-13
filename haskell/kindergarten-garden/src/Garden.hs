ec5698cb110c6d67e174fcd91113684246aeea62
Himanshu Ojha <himanshuo@gmail.com>
2018-01-13 17:48:16 -0500

    kindergarden

--- haskell/kindergarten-garden/src/Garden.hs
+++ haskell/kindergarten-garden/src/Garden.hs
@@ -6,57 +6,59 @@ module Garden
     ) where
 
 import Data.Char (toUpper)
-import Data.Map (Map, empty, insert)
+import Data.Map (Map, empty, insert, lookup)
+import Data.List (elemIndex, sort)
 data Plant = Clover
            | Grass
            | Radishes
            | Violets
            deriving (Eq, Show)
 
-
--- "Alice", "Bob", "Charlie", "David", "Eve", "Fred", "Ginny", "Harriet", "Ileana", "Joseph", "Kincaid", "Larry"
-
--- RC\nGG : Alice -> [R, C, G, G]
--- VVCG\nVVRC : Alice -> [V, V, V, V], Bob -> [C G R C]
-
-
-
-defaultGarden :: String -> Map String [Plant]
-defaultGarden plants = 
-    let map = Data.Map.empty in 
-    let students = ["Alice", "Bob", "Charlie", "David", "Eve", "Fred", "Ginny", "Harriet", "Ileana", "Joseph", "Kincaid", "Larry"] in 
-    let plants' = lines plants in 
-    foldl (updateMap plants' students) map [0..(length students)]
-
-    
-
-
-updateMap :: [String] -> [String] -> Map String [Plant] -> Int -> Map String [Plant]
-updateMap plants students map studentIndex = 
-    let studentsPlantsAsStrings = getStudentsPlantsAtIndex (studentIndex*2) ++  (getStudentsPlantsAtIndex (studentIndex * 2 + 1)) in
-    let studentsPlants = foldl addPlants [] studentsPlantsAsStrings in
-    insert (students !! studentIndex) studentsPlants map
-
-getStudentsPlantsAtIndex :: Int -> [String] -> [Plant]
-getStudentsPlantsAtIndex index rowsOfPlants =  foldl (\myplants curRow -> myplants ++ [curRow !! index] ) [] rowsOfPlants in 
-        
-addPlants :: [Plant] -> Char -> [Plant] 
-addPlants listOfPlants plantChr = 
-    let upperPlantChr = toUpper plantChr in 
-    case chrToPlant upperPlantChr of 
-        Left _ -> listOfPlants         
-        Right plant -> listOfPlants ++ [plant]
-
-
-chrToPlant :: Char -> Either String Plant 
+chrToPlant :: Char -> Either String Plant
 chrToPlant 'C' = Right Clover
 chrToPlant 'G' = Right Grass
 chrToPlant 'R' = Right Radishes
 chrToPlant 'V' = Right Violets
 chrToPlant chr = Left $ "Invalid String in list of plants: " ++ [chr]
 
+getPlantsForStudent :: Int -> String -> [Plant]
+getPlantsForStudent studentIndex row =
+  let getPlantAtIndex index = chrToPlant (row !! index)
+      addOnlyPlantToList possiblePlant list = case possiblePlant of
+                                                  Left _ -> list
+                                                  Right plant -> list ++ [plant]
+      firstPlant = getPlantAtIndex (studentIndex * 2)
+      secondPlant = getPlantAtIndex (studentIndex * 2 + 1)
+  in
+      foldl (\acc possiblePlant -> addOnlyPlantToList possiblePlant acc) [] [firstPlant, secondPlant]
+
+
+defaultGarden :: String -> Map String [Plant]
+defaultGarden plants =
+  let defaultStudents = ["Alice", "Bob", "Charlie", "David", "Eve", "Fred", "Ginny", "Harriet", "Ileana", "Joseph", "Kincaid", "Larry"]
+      indexOfNewLine = Data.List.elemIndex '\n' plants
+      numRequiredStudents = case indexOfNewLine of
+                                Nothing -> length plants `quot` 2
+                                Just a -> a `quot` 2
+      students = take numRequiredStudents defaultStudents
+  in
+    garden students plants
+
+
 garden :: [String] -> String -> Map String [Plant]
-garden students plants = error "You need to implement this function."
+garden students plants =
+  let rows = lines plants
+      sortedStudents = sort students
+      studentIndexes = [0..(length sortedStudents - 1)]
+  in
+      foldl (\acc studentIndex ->
+              let student = sortedStudents !! studentIndex
+                  plantsForStudent = foldl (\ac row -> ac ++ (getPlantsForStudent studentIndex row)) [] rows
+              in
+                Data.Map.insert student plantsForStudent acc
+              ) Data.Map.empty studentIndexes
 
 lookupPlants :: String -> Map String [Plant] -> [Plant]
-lookupPlants student garden = error "You need to implement this function."
+lookupPlants student garden = case Data.Map.lookup student garden of
+                                  Nothing -> []
+                                  Just plants -> plants
